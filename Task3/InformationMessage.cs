0e7f9d4c50b1b5ddc24fffcda36563b7db521c35
sachis <135745443+S-Achi-S@users.noreply.github.com>
2023-09-15 21:27:28 +0300

    add Task3
    Problem with: Path.IsPathFullyQualified(path)

---
+++ Task3/InformationMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task3
+{
+
+    public class InformationMessage
+    {
+        public InformationMessage()
+        {
+        }
+    }
+
+}
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.IO;
 
 namespace Task3
 {
+
     class program
     {
+        private static string DirectoryPath { get; set; }
+
+        private static long DirectorySize { get; set; }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("---");
+            Console.WriteLine($"The path is: {DirectoryPath}");
+
+            Console.ReadKey();
+        }
+
+        public static void GetDirectoryPath()
+        {
+            var receivedData = string.Empty;
+            Console.WriteLine("Please input the path: ");
+            receivedData = GetDataFromConsole();
+
+            DirectoryPath = receivedData;
+        }
+
+        public static string GetDataFromConsole()
+        {
+            var receivedData = string.Empty;
+            receivedData = Console.ReadLine();
+            receivedData = Path.GetFullPath(receivedData);
+
+            return receivedData;
         }
     }
+
 }
