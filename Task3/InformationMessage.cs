35091d5204c23e5126037fa95b2a179a10ab5e4c
S-Achi-S <irosru@gmail.com>
2023-09-16 13:19:36 +0300

    Task3 - finished.

--- Task3/InformationMessage.cs
+++ Task3/InformationMessage.cs
@@ -5,8 +5,22 @@ namespace Task3
 
     public class InformationMessage
     {
-        public InformationMessage()
+        public static void RequestData() => Console.WriteLine("Input directory path which size do you want to know: ");
+
+        public static void DirectoryDataName() => Console.WriteLine("Input directory (folder) name: ");
+
+        public static void ShowErrorApplication() => Console.WriteLine("[ERROR] Some probles with the app. Restart the app.");
+
+        public static void ShowErrorMethod(in string message)
         {
+            Console.WriteLine($"[INFO] Method of the app, can't be finished! \n[INFO] {message}");
+        }
+
+        public static void ShowInformation(in string directoryPath, in long SizeMesasge, in int FileCounInfo)
+        {
+            Console.WriteLine($"\t Info about {directoryPath}:\n" +
+                              $"\t - The number of files (include) catalogs in directory are: {FileCounInfo}.\n" +
+                              $"\t - Directory size is: {SizeMesasge} byte.");
         }
     }
 
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,33 +6,145 @@ namespace Task3
 
     class program
     {
-        private static string DirectoryPath { get; set; }
-
-        private static long DirectorySize { get; set; }
-
-        public static void Main(string[] args)
+        private static int FileCount = 0;
+        
+        static void Main(string[] args)
         {
-            Console.WriteLine($"The path is: {DirectoryPath}");
+            var directoryPath = string.Empty;
+            
+            InformationMessage.RequestData();
+            directoryPath = GetData();
 
+            try
+            {
+                bool isPathNotNull = CheckDirectoryPathOnNullString(directoryPath);
+                bool isDirectoryExist = CheckDirectoryExist(directoryPath);
+                
+                if ((isPathNotNull) && (!isDirectoryExist))
+                {
+                    var receivedDataName = string.Empty;
+                    InformationMessage.DirectoryDataName();
+                    receivedDataName = GetData();
+
+                    CreateFolder(directoryPath, receivedDataName);
+                }
+                
+                if ( (isPathNotNull) && (isDirectoryExist) )
+                {
+                    GetNumberOfEntriesInDirectory(directoryPath);
+                    var directorySize = GetDirectorySize(directoryPath);
+                    var directoryEntriesCount = FileCount;
+                    InformationMessage.ShowInformation(directoryPath, directorySize, directoryEntriesCount);
+                    
+                    CleanDirectory(directoryPath);
+                    directorySize = GetDirectorySize(directoryPath);
+                    GetNumberOfEntriesInDirectory(directoryPath);
+                    directoryEntriesCount = FileCount;
+                    InformationMessage.ShowInformation(directoryPath, directorySize, directoryEntriesCount);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                InformationMessage.ShowErrorApplication();
+                throw;
+            }
+            
+            
             Console.ReadKey();
         }
 
-        public static void GetDirectoryPath()
+        private static string GetData() 
         {
             var receivedData = string.Empty;
-            Console.WriteLine("Please input the path: ");
             receivedData = GetDataFromConsole();
 
-            DirectoryPath = receivedData;
+            return receivedData;
         }
 
-        public static string GetDataFromConsole()
+        private static string GetDataFromConsole() 
         {
             var receivedData = string.Empty;
             receivedData = Console.ReadLine();
-            receivedData = Path.GetFullPath(receivedData);
 
             return receivedData;
+        }
+
+        private static bool CheckDirectoryPathOnNullString(in string directoryPath) 
+        {
+            var receivedData = string.Empty;
+            receivedData = directoryPath;
+            bool isPathNotNull = string.IsNullOrWhiteSpace(receivedData);
+
+            if (!isPathNotNull)
+            {
+                return true;
+            }
+            else
+            {
+                var error = "The input data is NULL.";
+                InformationMessage.ShowErrorMethod(error);
+                return false;
+            }
+        }
+        
+        private static bool CheckDirectoryExist(in string directoryPath) 
+        {
+            bool isDirectoryExist = Directory.Exists(directoryPath);
+
+            if (isDirectoryExist)
+            {
+                return true;
+            }
+            else
+            {
+                var error = "The folder not exist.";
+                InformationMessage.ShowErrorMethod(error);
+                
+                return false;
+            }
+        }
+
+        private static void CreateFolder(in string directoryPath, in string directoryName) 
+        {
+            bool isDirectoryExist = CheckDirectoryExist(directoryPath);
+            DirectoryInfo workDirectory = new DirectoryInfo(directoryPath);
+            
+            if (isDirectoryExist)
+                workDirectory.Create();
+
+            workDirectory.CreateSubdirectory(directoryName);
+        }
+
+        private static long GetDirectorySize(in string directoryPath)
+        {
+            long directorySize = 0;
+            string[] pathsList = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+
+            foreach (var filePath in pathsList)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                directorySize += fileInfo.Length;
+            }
+
+            return directorySize;
+        }
+
+        private static void GetNumberOfEntriesInDirectory(in string directoryPath)
+        {
+            FileCount += Directory.GetFiles(directoryPath).Length;
+
+            foreach (string subdirectory in Directory.GetDirectories(directoryPath))
+            {
+                GetNumberOfEntriesInDirectory(subdirectory);
+            } 
+        }
+        
+        private static void CleanDirectory(in string DirectoryName)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(DirectoryName);
+            dirInfo.Delete(true);
+            Console.WriteLine("Directory has been deleted.");
         }
     }
 
