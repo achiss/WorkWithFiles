7bfed7184d2ba250f5b2537e573012a5b670b1bb
sachis <135745443+S-Achi-S@users.noreply.github.com>
2023-09-16 22:13:17 +0300

    Init - Task4

---
+++ FinalTask/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace FinalTask
+{
+
+    class MainClass 
+    {
+        private static string StudentName { get; set; }
+        private static string StudentGroup { get; set; }
+        private static DateTime BirthDate { get; set; }
+
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine($"Please, input the path to file: ");
+
+            var receivedPathToFile = string.Empty;
+            receivedPathToFile = GetPathToFile();
+
+            Console.WriteLine($"+++ {receivedPathToFile}");
+        }
+
+        private static string GetPathToFile()           //path not string
+        {
+            var receivedData = string.Empty;
+            receivedData = GetPathFromConsole();
+
+            bool CheckFileExist = IsFileExist(receivedData);
+            bool CheckNullData = GetPathToFileNotNull(receivedData);
+
+            if ( (!CheckFileExist) && (CheckNullData) )
+            {
+                Console.WriteLine($"[INFO] {receivedData}\n[INFO] Not correct data.\nRepeat input data one more time: ");
+                GetPathToFile();
+            }
+
+            return receivedData;
+        }
+
+        private static string GetPathFromConsole() 
+        {
+            var receivedData = string.Empty;
+            receivedData = Path.GetFullPath(Console.ReadLine());
+
+            return receivedData;
+        }
+
+        private static bool IsFileExist(in string dataToCheck)
+        {
+            if (!File.Exists(dataToCheck))
+            {
+                Console.WriteLine($"[INFO] It cann't to read the file using received path!");
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static bool GetPathToFileNotNull(in string dataToCheck)
+        {
+            if (string.IsNullOrWhiteSpace(dataToCheck))
+            {
+                Console.WriteLine($"[INFO] Received path is null or contain something!");
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+}
